adc4a97698cf25fedb6a059ea580dce334730af8
Mahsa <m.hesamifar@yahoo.com>
2024-06-29 16:26:33 +0330

    feat: Displaying categories in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,54 @@
 "use client";
 
+import Image from "next/image";
+
 import { PostBox } from "@/components/elements";
+import { useGetCategoriesQuery } from "@/services/category";
 import { useGetAllPostsQuery } from "@/services/post";
 
 export default function Home() {
-  const { data, isLoading } = useGetAllPostsQuery();
+  const { data: postsData, isLoading: postsIsLoading } = useGetAllPostsQuery();
+  const { data: categoriesData, isLoading: categoriesIsLoading } =
+    useGetCategoriesQuery();
 
   return (
-    <main className="flex flex-col items-center justify-between py-10 px-5">
-      {isLoading ? (
+    <main className="flex flex-col items-center justify-between xl:flex-row xl:items-start py-10 px-5 xl:px-60">
+      {postsIsLoading || categoriesIsLoading ? (
         <>Loading...</>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-full xl:w-2/3">
-          {data &&
-            data.posts?.map((post, index) => {
-              return <PostBox post={post} key={index} />;
-            })}
-        </div>
+        <>
+          <div className="w-1/4">
+            <div className="text-sm mb-4">دسته ها</div>
+            <div>
+              {categoriesData?.map((category, index) => {
+                const iconSrc = require(`@/icons/${category.icon}.svg`).default;
+                return (
+                  <div
+                    key={index}
+                    className="text-grey-400 py-2 flex items-center"
+                  >
+                    <Image
+                      className="ml-2"
+                      src={iconSrc}
+                      alt={category.name}
+                      width={30}
+                      height={30}
+                      priority
+                    />
+                    {category.name}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-full xl:w-3/4">
+            {postsData &&
+              postsData.posts?.map((post, index) => {
+                return <PostBox post={post} key={index} />;
+              })}
+          </div>
+        </>
       )}
     </main>
   );
